bb9f5dcd65681bee4626e531f8ac8a959d5fb36e
AzraelGarden <azraelgarden@gmail.com>
2020-03-25 00:53:11 -0300

    Backend finalizado.

---
+++ backend/src/database/migrations/20200324233823_create_ongs.js
@@ -0,0 +1,18 @@
+//Metódo UP: Responsável pela criação
+exports.up = function(knex) {
+return knex.schema.createTable('ongs', function(table){
+      table.string('id').primary();
+
+      table.string('name').notNullable();
+      table.string('email').notNullable();
+      table.string('whatsapp').notNullable();
+      table.string('city').notNullable();
+      table.string('uf', 2).notNullable();
+
+  })
+};
+
+//Método Down: Deleção da Tabela
+exports.down = function(knex) {
+    return knex.schema.dropTable('ongs');
+};
---
+++ backend/src/database/migrations/20200324234704_create_incidents.js
@@ -0,0 +1,20 @@
+//Metódo UP: Responsável pela criação
+exports.up = function(knex) {
+    return knex.schema.createTable('incidents', function(table){
+          table.increments();
+
+          table.string('title').notNullable();
+          table.string('description').notNullable();
+          table.decimal('value').notNullable();
+
+          table.string('ong_id').notNullable();
+
+          table.foreign('ong_id').references('id').inTable('ongs');
+      })
+    };
+    
+    //Método Down: Deleção da Tabela
+    exports.down = function(knex) {
+      return knex.schema.dropTable('incidents');
+    };
+    
